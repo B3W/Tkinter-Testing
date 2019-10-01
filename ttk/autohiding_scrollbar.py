49b683328988d9663e720cc42af13bd0892c111d
B3W <westieberg@gmail.com>
2019-09-30 21:38:58 -0500

    Utilizing widget.grid_forget() method

--- ttk/autohiding_scrollbar.py
+++ ttk/autohiding_scrollbar.py
@@ -37,8 +37,7 @@ class AutoScrollbar(ttk.Scrollbar):
 #     '''
 #     def set(self, lo, hi):
 #         if float(lo) <= 0.0 and float(hi) >= 1.0:
-#             # grid_remove is currently missing from Tkinter!
-#             self.tk.call("grid", "remove", self)
+#             self.grid_forget()
 #         else:
 #             self.grid()
 
